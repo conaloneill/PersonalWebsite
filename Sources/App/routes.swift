459ed8c6c154f3fa32db2055705e49724432009b
Conal O'Neill <conal.oneill95@gmail.com>
2018-05-21 17:48:22 +0100

    added base projects route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -10,38 +10,45 @@ struct MainView: Codable {
 	var title: String?
 	var body: String?
 }
+struct ProjectView: Codable {
+	var name: String?
+	var description: String?
+	var photo: String?
+	var allProjects: [String]
+}
+
+let projects = [
+	"monopoly": "Desktop app Monopoly game built in Java",
+	"monopolyBot": "Desktop app Monopoly game bot built in Java",
+	"panopoly": "variation on Monopoly running on AWS with android and desktop app components",
+	"website": "Personal website built on Vapor 3.0.2, written in Swift and hosted on Heroku"
+]
+
+
 public func routes(_ router: Router) throws {
     router.get("/") { req -> Future<View> in
 		
-		let context = MainView(title: "Welcome to my personal website!", body: "This website is running on Vapor 3.0.2 and hosted on GitHub Pages.")
+		let context = MainView(title: "Welcome to my personal website!", body: "This website is running on Vapor 3.0.2 and hosted on Heroku.")
         return try req.view().render("home", context)
     }
 	
 	
 	router.get("project", String.parameter) { req -> Future<View> in
 		let projectName = try req.parameters.next(String.self)
-
-		
-		let projects = [
-			"monopoly": "Desktop app Monopoly game built in Java",
-			"monopolyBot": "Desktop app Monopoly game bot built in Java",
-			"website": "Personal website built on Vapor 3.0.2 written in Swift"
-		]
-		struct ProjectView: Codable {
-			var name: String?
-			var description: String?
-			var photo: String?
-			var allProjects: [String]?
-		}
 		
 		let context: ProjectView
 		
-		if let projectDescription = projects[projectName] {
-			context = ProjectView(name: projectName, description: projectDescription, photo: nil, allProjects: projects.keys.sorted())
+		if let project = projects[projectName] {
+			context = ProjectView(name: projectName, description: project, photo: projectName, allProjects: projects.keys.sorted())
 		} else {
 			context = ProjectView(name: nil, description: nil, photo: nil, allProjects: projects.keys.sorted())
 		}
 		
+		return try req.view().render("project", context)
+	}
+	
+	router.get("project") { req -> Future<View> in
+		let context = ProjectView(name: nil, description: nil, photo: nil, allProjects: projects.keys.sorted())
 		return try req.view().render("project", context)
 	}
 	
