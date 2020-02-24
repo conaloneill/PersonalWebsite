93e71654811e3876ce826b14bc1786882707c46e
conaloneill <conaloneill@users.noreply.github.com>
2020-02-24 15:29:40 +0000

    initial Vapor 4 setup

--- Package.swift
+++ Package.swift
@@ -1,18 +1,26 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
     name: "PersonalWebsite",
+    platforms: [
+       .macOS(.v10_14)
+    ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-		.package(url: "http://github.com/vapor/leaf.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta"),
+		.package(url: "http://github.com/vapor/leaf.git", from: "4.0.0-beta"),
 		.package(url: "https://github.com/conaloneill/MailCore.git", .branch("master")),
+        
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "Leaf", "MailCore"]),
+        .target(name: "App", dependencies: [
+            "Vapor",
+            "Leaf",
+            "MailCore"
+        ]),
         .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App", "XCTVapor"]),
     ]
 )
 
